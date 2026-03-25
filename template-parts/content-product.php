39cd88e1c1073cdb446d4cabe6325a68d243fd93
Didier Grand <didier.grand@gmail.com>
2026-03-25 16:24:59 +0100

    Add badges

--- template-parts/content-product.php
+++ template-parts/content-product.php
@@ -8,11 +8,42 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'home-post produits-post' ); ?>>
+	<?php
+	$badge_type = get_post_meta( get_the_ID(), 'badge_type', true );
+	if ( empty( $badge_type ) ) {
+		$badge_type = get_post_meta( get_the_ID(), 'badge', true );
+	}
+	if ( empty( $badge_type ) ) {
+		$badge_type = get_post_meta( get_the_ID(), 'produit_badge', true );
+	}
+
+	$badge_type = ! empty( $badge_type ) ? sanitize_title( (string) $badge_type ) : '';
+
+	$badge_label = get_post_meta( get_the_ID(), 'badge_label', true );
+	if ( empty( $badge_label ) ) {
+		$badge_label = get_post_meta( get_the_ID(), 'badge_texte', true );
+	}
+
+	if ( empty( $badge_label ) ) {
+		if ( $badge_type === 'nouveau' ) {
+			$badge_label = 'Nouveau';
+		} elseif ( $badge_type === 'promo' ) {
+			$badge_label = 'Promo';
+		}
+	}
+	?>
+
 	<?php if ( has_post_thumbnail() ) : ?>
 		<div class="home-post__thumbnail">
 			<a href="<?php the_permalink(); ?>">
 				<?php the_post_thumbnail( 'medium_large' ); ?>
 			</a>
+
+			<?php if ( ! empty( $badge_label ) && ! empty( $badge_type ) ) : ?>
+				<span class="product-badge product-badge--<?php echo esc_attr( $badge_type ); ?>">
+					<?php echo esc_html( $badge_label ); ?>
+				</span>
+			<?php endif; ?>
 		</div>
 	<?php endif; ?>
 
