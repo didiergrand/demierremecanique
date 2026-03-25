fec32c2bc3439b54cb54b3c63564592319d6837d
Didier Grand <didier.grand@gmail.com>
2026-03-25 09:54:10 +0100

    Fix image display

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,16 +10,7 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	<?php
-	if ( have_posts() ) :
-		while ( have_posts() ) :
-			the_post();
-			demierre_mecanique_display_header_image( '', false );
-			break; // Only need to display header once
-		endwhile;
-		rewind_posts();
-	endif;
-	?>
+	<?php demierre_mecanique_post_thumbnail(); ?>
 	<div class="entry-content">
 		<?php
 		the_content();
